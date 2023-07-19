835b6dada57c5764dadb6eb03075c94900d55fd9
Reed Patterson <reed.patterson@angeltrax.com>
2023-07-19 15:48:09 -0500

    rough draft

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,18 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "migrationtimestamp" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('migrationtimestamp.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MigrationTimestamp!');
+	let disposable = vscode.commands.registerCommand('migrationtimestamp.printTimestamp', async () => {
+		const x = new Date;
+		const applyLeadingZero = (val: number) => (`0${val}`).slice(-2);
+		const test = `${x.getFullYear()}${applyLeadingZero(x.getMonth() + 1)}${applyLeadingZero(x.getDate())}${applyLeadingZero(x.getHours())}${applyLeadingZero(x.getMinutes())}`;
+		// await vscode.env.clipboard.writeText(test);
+		if (vscode.window.activeTextEditor?.selection?.active) {
+			vscode.TextEdit.insert(vscode.window.activeTextEditor?.selection?.active, test);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
