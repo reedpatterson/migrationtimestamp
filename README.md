fa4d2967d521e52e6154a9201397fc1dd8c3d72b
Reed Patterson <reed.patterson@angeltrax.com>
2023-07-19 16:48:21 -0500

    include second print method; asign hotkeys

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A VSCode extension to output a Date/Timestamp formatted for use in ASP.NET Core 
 
 ## Features
 
-Press Ctrl + Alt + D to print a timestamp.
+Press Ctrl + Shift + J to print a local migration timestamp.
+Press Ctrl + Shift + K to print a local timestamp in YYYY-MM-DDTHH:mm:ss format (seconds rounded down to 00).
--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,29 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerCommand('migrationtimestamp.printTimestamp', async () => {
+	let disposable = vscode.commands.registerTextEditorCommand('migrationtimestamp.printMigrationTimestamp', (editor, edit) => {
 		const x = new Date;
 		const applyLeadingZero = (val: number) => (`0${val}`).slice(-2);
-		const test = `${x.getFullYear()}${applyLeadingZero(x.getMonth() + 1)}${applyLeadingZero(x.getDate())}${applyLeadingZero(x.getHours())}${applyLeadingZero(x.getMinutes())}`;
-		// await vscode.env.clipboard.writeText(test);
-		if (vscode.window.activeTextEditor?.selection?.active) {
-			vscode.TextEdit.insert(vscode.window.activeTextEditor?.selection?.active, test);
-		}
+		const migrationTimestamp = `${x.getFullYear()}${applyLeadingZero(x.getMonth() + 1)}${applyLeadingZero(x.getDate())}${applyLeadingZero(x.getHours())}${applyLeadingZero(x.getMinutes())}`;
+		// await vscode.env.clipboard.writeText(migrationTimestamp);
+		editor.selections.forEach((selection, i) => {
+			let text = migrationTimestamp + i;
+			edit.insert(selection.active, text);
+		});
 	});
 
-	context.subscriptions.push(disposable);
+	let disposable2 = vscode.commands.registerTextEditorCommand('migrationtimestamp.printTimestamp', (editor, edit) => {
+		const x = new Date;
+		const applyLeadingZero = (val: number) => (`0${val}`).slice(-2);
+		const timestamp = `${x.getFullYear()}-${applyLeadingZero(x.getMonth() + 1)}-${applyLeadingZero(x.getDate())}T${applyLeadingZero(x.getHours())}:${applyLeadingZero(x.getMinutes())}:00`;
+		console.log(timestamp);
+		editor.selections.forEach((selection, i) => {
+			let text = timestamp + i;
+			edit.insert(selection.active, text);
+		});
+	});
+
+	context.subscriptions.push(disposable, disposable2);
 }
 
 export function deactivate() {}
