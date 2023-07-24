d22f3ab7cdc13997da44bc24f081071d7e4cc61b
Reed Patterson <reed.patterson@angeltrax.com>
2023-07-24 11:38:47 -0500

    remove unnecessary index appending

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,8 @@ export function activate(context: vscode.ExtensionContext) {
 		const applyLeadingZero = (val: number) => (`0${val}`).slice(-2);
 		const migrationTimestamp = `${x.getFullYear()}${applyLeadingZero(x.getMonth() + 1)}${applyLeadingZero(x.getDate())}${applyLeadingZero(x.getHours())}${applyLeadingZero(x.getMinutes())}`;
 		// await vscode.env.clipboard.writeText(migrationTimestamp);
-		editor.selections.forEach((selection, i) => {
-			let text = migrationTimestamp + i;
+		editor.selections.forEach((selection) => {
+			let text = migrationTimestamp;
 			edit.insert(selection.active, text);
 		});
 	});
@@ -16,9 +16,8 @@ export function activate(context: vscode.ExtensionContext) {
 		const x = new Date;
 		const applyLeadingZero = (val: number) => (`0${val}`).slice(-2);
 		const timestamp = `${x.getFullYear()}-${applyLeadingZero(x.getMonth() + 1)}-${applyLeadingZero(x.getDate())}T${applyLeadingZero(x.getHours())}:${applyLeadingZero(x.getMinutes())}:00`;
-		console.log(timestamp);
-		editor.selections.forEach((selection, i) => {
-			let text = timestamp + i;
+		editor.selections.forEach((selection) => {
+			let text = timestamp;
 			edit.insert(selection.active, text);
 		});
 	});
